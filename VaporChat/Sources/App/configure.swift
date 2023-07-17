f1ac9e631d1c56a4715a4dea7a82212f7b51c46c
Viktor <vinill2@ukr.net>
2023-07-17 09:34:48 +0300

    change db to postgreql, change db configure

--- VaporChat/Sources/App/configure.swift
+++ VaporChat/Sources/App/configure.swift
@@ -1,13 +1,19 @@
 import Vapor
 import Fluent
-import FluentSQLiteDriver
+import FluentPostgresDriver
 
 // configures your application
 public func configure(_ app: Application) async throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
+    app.databases.use(.postgres(
+        hostname: "localhost",
+        port: 5432,
+        username: "a12345",
+        password: "",
+        database: "postgres"
+    ), as: .psql)
 
-    app.databases.use(.sqlite(.file("db.sqlite")), as: .sqlite)
     app.migrations.add(CreateUser())
     try await app.autoMigrate()
     app.logger.logLevel = .debug
